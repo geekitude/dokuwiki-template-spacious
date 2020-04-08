c82bddc01e12a7f8e8802ad26fd536a1d2e70079
Simon DELAGE <sdelage@gmail.com>
2020-04-08 14:45:06 +0200

    Introduced Spacious CSS

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+
+        // Reference package.json
+        pkg: grunt.file.readJSON('package.json'),
+
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('autoprefixer')({
+                        overrideBrowserlist: ['last 2 versions']
+                    }),
+                    require('postcss-rtl')()
+                ]
+            },
+            dist: {
+                files: {
+                    'css/spacious.dokuwiki.less': ['css/src/spacious.dokuwiki.less'],
+                    'css/spacious.less': ['css/src/spacious.less'],
+                    'css/spacious.plugins.less': ['css/src/spacious.plugins.less'],
+                    'css/spacious.print.css': ['css/src/spacious.print.css'],
+                    'css/spacious.theme.less': ['css/src/spacious.theme.less']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'css/src/*.*',
+                tasks: ['postcss'],
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+};
